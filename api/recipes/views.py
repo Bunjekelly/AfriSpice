4bf61d966a4b888f11184424451aecec52cea904
Bunje kelly <kellybunje@gmail.com>
2024-03-29 16:41:10 +0100

    added views that will allow me perform CRUD operations on my recipes and ingredient models(classes)

--- api/recipes/views.py
+++ api/recipes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Recipe, Ingredient
+from .serializers import RecipeSerializer, IngredientSerializer
 
-# Create your views here.
+class RecipeListCreateView(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+class RecipeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+class IngredientListCreateView(generics.ListCreateAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+class IngredientRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
