82ff4f4c74a38f3040c7614617a33214834f75c4
Bunje kelly <kellybunje@gmail.com>
2024-03-29 16:11:17 +0100

    added models for api.recipes, included class for recipes and class for ingredients

---
+++ api/recipes/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    cook_time = models.TimeField()
+    ingredients = models.ManyToManyField('Ingredient', related_name='recipes')
+    procedure = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
