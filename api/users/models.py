92bdf2149fde60d04e7d5943fd96faff3455f4bd
Bunje kelly <kellybunje@gmail.com>
2024-03-31 12:04:55 +0100

    added the UserProfile Class

--- api/users/models.py
+++ api/users/models.py
@@ -7,4 +7,9 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200, blank=True)
 
+    def __str__(self):
+        return self.user.username
