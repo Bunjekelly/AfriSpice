7b9cc1e2ef8a7cda7fa3520cba1ee980208e2227
Bunje kelly <kellybunje@gmail.com>
2024-03-31 12:20:26 +0100

    registered User and Profile in admin site

--- api/users/urls.py
+++ api/users/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='user-login'),
     path('logout/', views.UserLogoutView.as_view(), name='user-logout'),
     path('profile/', views.UserProfileView.as_view(), name='user-profile'),
-    path('password-change/', views.UserPasswordChangeView.as_view(), name='password-change'),
 ]
